e3a464ad77225d209036b19fa6206d3301216b44
Simon Josefsson <simon@josefsson.org>
2012-01-03 14:07:02 +0100

    Add OCSP verify example.

--- doc/examples/ex-ocsp-client.c
+++ doc/examples/ex-ocsp-client.c
@@ -15,11 +15,9 @@
 /* This program will read a file (argv[1]) containing a certificate in
    PEM format and print the "CA issuers" and "OCSP address" extensions
    for the certificate.  If another file is given (argv[2]) it holds
-   the issuer certificate for the first certificate, and another file
-   (argv[3]) should contain a set of trust anchors in PEM format.
-   Then the tool will generate an OCSP request and will read an OCSP
-   response from standard input and verify it against the trust
-   anchors. */
+   the issuer certificate for the first certificate.  Then the tool
+   will generate an OCSP request for the certificate and write it to
+   the file "ocsp-req.der". */
 
 int
 main (int argc, char *argv[])
@@ -29,6 +27,8 @@ main (int argc, char *argv[])
   gnutls_datum_t certdata, issuerdata, tmp;
   size_t s;
   unsigned int seq;
+  gnutls_ocsp_req_t ocspreq = NULL;
+  FILE *fh;
 
   rc = gnutls_global_init ();
   if (rc < 0)
@@ -107,7 +107,7 @@ main (int argc, char *argv[])
       goto done;
     }
 
-  /* Read issuer and print OCSP request. */
+  /* Read issuer cert and print brief info about it. */
 
   rc = gnutls_x509_crt_init (&issuer);
   if (rc < 0)
@@ -134,11 +134,57 @@ main (int argc, char *argv[])
 
   gnutls_free (tmp.data);
 
+  /* Generate OCSP request and write it. */
+
+  rc = gnutls_ocsp_req_init (&ocspreq);
+  if (rc < 0)
+    goto done;
+
+  rc = gnutls_ocsp_req_add_cert (ocspreq, GNUTLS_DIG_SHA1, issuer, cert);
+  if (rc < 0)
+    goto done;
+
+  rc = gnutls_ocsp_req_print (ocspreq, GNUTLS_OCSP_PRINT_FULL, &tmp);
+  if (rc < 0)
+    goto done;
+
+  printf ("ocsp request: %.*s\n", tmp.size, tmp.data);
+
+  gnutls_free (tmp.data);
+
+  fh = fopen ("ocsp-req.der", "w");
+  if (fh == NULL)
+    goto done;
+
+  rc = gnutls_ocsp_req_export (ocspreq, &tmp);
+  if (rc < 0)
+    goto done;
+
+  s = fwrite (tmp.data, 1, tmp.size, fh);
+
+  gnutls_free (tmp.data);
+
+  if (s != tmp.size)
+    {
+      perror ("fwrite");
+      fclose (fh);
+      goto done;
+    }
+
+  rc = fclose (fh);
+  if (rc != 0)
+    {
+      perror ("fclose");
+      rc = 0;
+      goto done;
+    }
+
   rc = 0;
 
  done:
   if (rc != 0)
     printf ("error (%d): %s\n", rc, gnutls_strerror (rc));
+  gnutls_ocsp_req_deinit (ocspreq);
   gnutls_x509_crt_deinit (cert);
   gnutls_x509_crt_deinit (issuer);
   gnutls_global_deinit ();
